db1d0f98d99415a9a1eb1af2b30347e9bd5f7e9c
Kaishu <kaishusahu101@gmail.com>
2018-02-06 17:45:13 +0530

    FriendListActivityAdded

--- app/src/main/java/com/rancon/SendCard.java
+++ app/src/main/java/com/rancon/SendCard.java
@@ -13,131 +13,113 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.facebook.AccessToken;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
+import com.facebook.login.widget.ProfilePictureView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.security.auth.login.LoginException;
 
 public class SendCard extends AppCompatActivity {
     RecyclerView friendRecycler;
     List<String> friendNameList = new ArrayList<>();
     List<String> friendIdList = new ArrayList<>();
+
+    friendAdapter friendAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_card);
         friendRecycler = (RecyclerView) findViewById(R.id.friend_recycler);
         friendRecycler.setLayoutManager(new LinearLayoutManager(this));
-        DwnFriendList dwn = new DwnFriendList();
-        dwn.execute();
+        friendAdapter = new friendAdapter(this);
+        dwnFriendList();
+        Log.e("fasddsa","sdfsad");
 
     }
 
     public class friendAdapter extends RecyclerView.Adapter<friendHolder> {
         LayoutInflater lf;
-        List<String> nameList;
-        List<String> idList;
 
-        public friendAdapter(Context c, List<String> nameList, List<String> idList) {
+        public friendAdapter(Context c) {
             this.lf = LayoutInflater.from(c);
-            this.nameList = nameList;
-            this.idList = idList;
         }
 
         @Override
         public friendHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View v = lf.inflate(R.layout.card_element, parent, false);
+            View v = lf.inflate(R.layout.friends_element, parent, false);
             friendHolder fiendHolder = new friendHolder(v);
             return fiendHolder;
         }
 
         @Override
         public void onBindViewHolder(final friendHolder holder, final int position) {
-            holder.tV.setText(nameList.get(position));
-            @SuppressLint("StaticFieldLeak")
-            AsyncTask dwmPic = new AsyncTask() {
-                @Override
-                protected Object doInBackground(Object[] objects) {
-                    try {
-                        URL imgEnc = new URL("http://graph.facebook.com/" + idList.get(position) + "/picture?type=small");
-                        Bitmap picture = BitmapFactory.decodeStream(imgEnc.openConnection().getInputStream());
-                        holder.iV.setImageBitmap(picture);
-                    } catch (Exception e) {
-
-                    }
-                    return null;
-                }
-            };
-            dwmPic.execute();
-
+            Log.e("TEST",""+position);
+            holder.tV.setText(friendNameList.get(position));
+            holder.iV.setProfileId(friendIdList.get(position));
         }
 
         @Override
         public int getItemCount() {
-            return 5;
+            Log.e("TEST12321",""+friendNameList.size());
+            return friendNameList.size();
         }
     }
 
     public class friendHolder extends RecyclerView.ViewHolder {
         TextView tV;
-        ImageView iV;
+        ProfilePictureView iV;
 
         public friendHolder(View itemView) {
             super(itemView);
             tV = (TextView) itemView.findViewById(R.id.friend_name);
-            iV = (ImageView) itemView.findViewById(R.id.profile_pic);
+            iV = (ProfilePictureView) itemView.findViewById(R.id.profile_pic);
         }
     }
 
-    private class DwnFriendList extends AsyncTask {
-        @Override
-        protected Object doInBackground(Object[] objects) {
-            String uId = AccessToken.getCurrentAccessToken().getUserId();
-            final GraphRequest request = GraphRequest.newGraphPathRequest(
-                    AccessToken.getCurrentAccessToken(),
-                    "/"+uId+"/friends",
-                    new GraphRequest.Callback() {
-                        @Override
-                        public void onCompleted(GraphResponse response) {
-                            Log.e("result", response.getJSONObject().toString());
-                            try {
-                                JSONArray data = response.getJSONObject().getJSONArray("name");
-                                for (int i = 0 ; i < data.length() ; i++) {
-                                    JSONObject friendDetail = data.getJSONObject(i);
-                                    String name = friendDetail.getString("name");
-                                    String id = friendDetail.getString("id");
-                                    friendNameList.add(name);
-                                    friendIdList.add(id);
-                                }
+    private void dwnFriendList() {
 
-                            } catch (JSONException e) {
-                                e.printStackTrace();
+        String uId = AccessToken.getCurrentAccessToken().getUserId();
+        final GraphRequest request = GraphRequest.newGraphPathRequest(
+                AccessToken.getCurrentAccessToken(),
+                "/" + uId + "/friends",
+                new GraphRequest.Callback() {
+                    @Override
+                    public void onCompleted(GraphResponse response) {
+                        Log.e("result", response.getJSONObject().toString());
+                        try {
+                            JSONArray data = response.getJSONObject().getJSONArray("data");
+                            for (int i = 0; i < data.length(); i++) {
+                                JSONObject aa = data.getJSONObject(i);
+                                String name = aa.getString("name");
+                                String id = aa.getString("id");
+                                friendNameList.add(name);
+                                friendIdList.add(id);
+                                Log.e("TESTqq", name);
+                                Log.e("TESTqq", id);
                             }
+                            friendRecycler.setAdapter(friendAdapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
-                    });
-
-            request.executeAsync();
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Object o) {
-            super.onPostExecute(o);
-        }
-
+                    }
+                });
+        request.executeAsync();
     }
+
 
 }
